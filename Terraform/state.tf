ee25b239d99f632588e8cec5485f4b3f027ae191
Ian Hay <techsavhay@gmail.com>
2026-07-06 15:22:53 +0100

    added s3 bucket in state.tf to hold state file

--- Terraform/state.tf
+++ Terraform/state.tf
@@ -1,1 +1,17 @@
 # IAC for the VetOps AWS resources
+
+#====================================
+#terraform state bucket for state.tf
+#======================================
+
+resource "aws_s3_bucket" "terraform_state" { bucket = "vet-hospital-tf-state-unique-id"}
+
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    {
+        status = "Enabled"
+    }
+  }
+}
