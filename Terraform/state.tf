b01b46f7db82d144305f1094d8f295329b9bdf5c
Ian Hay <techsavhay@gmail.com>
2026-07-07 08:51:28 +0100

    updated gitignore

--- Terraform/state.tf
+++ Terraform/state.tf
@@ -1,4 +1,4 @@
-# IAC for the VetOps AWS resources
+# IAC for the VetOps AWS resources ()
 
 
 
@@ -23,3 +23,4 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
 resource "aws_s3_bucket" "alb_logs" {
     bucket = "vetop-vet-hospital-alb-logs"
 }
+
