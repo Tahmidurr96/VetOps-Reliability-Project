8e1a3ca27bf9f8e7f4be573c56c2ad73dc1952a8
Ian Hay <techsavhay@gmail.com>
2026-07-07 11:15:33 +0100

    commented out extra dynamodb line

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -9,8 +9,8 @@ terraform {
     bucket         = "vetop-vet-hospital-tf-state"
     key            = "infrastructure/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "vetop-vet-hospital-tf-lock"
-    use_lockfile = true
+   #dynamodb_table = "vetop-vet-hospital-tf-lock"
+    use_lockfile   = true
 
   }
 }
