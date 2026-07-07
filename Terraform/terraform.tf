5f49282a850567e9f49e66a23218776c7ac8b809
Chris Adams <cn10cra@gmailcom>
2026-07-07 16:06:25 +0100

    tf fmt

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -6,11 +6,11 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "vetop-vet-hospital-tf-state"
-    key            = "infrastructure/terraform.tfstate"
-    region         = "eu-west-2"
-   #dynamodb_table = "vetop-vet-hospital-tf-lock"
-    use_lockfile   = true
+    bucket = "vetop-vet-hospital-tf-state"
+    key    = "infrastructure/terraform.tfstate"
+    region = "eu-west-2"
+    #dynamodb_table = "vetop-vet-hospital-tf-lock"
+    use_lockfile = true
 
   }
 }
