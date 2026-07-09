39bdc021e68566751abad3dc861aa77266e689a5
Ian Hay <techsavhay@gmail.com>
2026-07-09 15:53:08 +0100

    includes code for elasticache and lambda security groups

--- Terraform/network-security.tf
+++ Terraform/network-security.tf
@@ -22,3 +22,21 @@ egress {
     cidr_blocks = "0.0.0.0/0"
 }
 }
+
+#==========================================
+# A SECURITY GROUP FOR THE ELASTICACHE
+#==========================================
+
+resource "aws_security_group" "elasticache_sg" {
+    name = "vet-cache-redis-sg"
+    vpc_id = "vpc-080dbb0b7dc86503a"
+
+ingress {
+    from_port = 6379
+    to_port = 6379
+    protocol = "tcp"
+    # this line allows only our lambda to connect to this cache
+    security_groups = [aws_security_group.lambda_sg.id]
+}
+
+}
