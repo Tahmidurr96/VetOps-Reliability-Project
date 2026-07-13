c5b4044c4edfde65adf7a8621df45ff4219b8920
Ian Hay <techsavhay@gmail.com>
2026-07-13 09:00:17 +0100

    added in correct api url, 15 second timeout and simplified outbound rules

--- Terraform/network-security.tf
+++ Terraform/network-security.tf
@@ -1,4 +1,4 @@
-#===========================
+#==========================================
 # A SECURITY GROUP FOR THE LAMBDA FUNCTION
 #==========================================
 
@@ -6,19 +6,11 @@ resource "aws_security_group" "lambda_sg" {
   name   = "vet-cache-lambda-sg"
   vpc_id = "vpc-080dbb0b7dc86503a"
 
-
-  # egress rule to allow lambda to talk to elasticache
+  # Explicitly allow all outbound internet traffic
   egress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # egress rule to let lambda to fetch data from the backend (if it isnt cached)
-  egress {
-    from_port   = 80
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -32,13 +24,12 @@ resource "aws_security_group" "elasticache_sg" {
   vpc_id = "vpc-080dbb0b7dc86503a"
 
   ingress {
-    from_port = 6379
-    to_port   = 6379
-    protocol  = "tcp"
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
     # this line allows only our lambda to connect to this cache
     security_groups = [aws_security_group.lambda_sg.id]
   }
-
 }
 
 #===============================
@@ -58,13 +49,11 @@ resource "aws_iam_role" "lambda_exec_role" {
       }
     }]
   })
-
 }
 
 #=====================================
-#IAM policy made by aws for lambdas running in a VPC
-#=================
-
+# IAM policy made by aws for lambdas running in a VPC
+#=====================================
 resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
@@ -73,15 +62,15 @@ resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
 # ==========================================
 # load balancer invocation Permission
 # ==========================================
-
 resource "aws_lambda_permission" "allow_alb" {
-  statement_id = "AllowExecutionFromALB"
-  action       = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromALB"
+  action        = "lambda:InvokeFunction"
 
   # yet to be defined/written, but named here for now
   function_name = aws_lambda_function.cache_proxy.function_name
   principal     = "elasticloadbalancing.amazonaws.com"
 
   # restricts access to least privelege.
-  source_arn = aws_lb_target_group.lambda_cache_tg.arn
+  source_arn    = aws_lb_target_group.lambda_cache_tg.arn
 }
+
